f383615bfefcdfeef6e37a4c83bfc5f4d2f1ea75
Awais Raees Ansari <80111573+AnsariAwaisRaees@users.noreply.github.com>
2023-11-28 12:47:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
-  
+	//write your code here
+	// return the min cost
+	let sum = 0;
+	let res = 0;
+
+	while(arr.length != 1){
+		arr.sort(
+			(a,b) => {
+				return a-b;
+			}
+
+			let sum = arr[0]+arr[1];
+			res += sum;
+			arr.splice(0,2,sum);
+			arr.sort();
+		);
+	}
+
+	return res;
 }
 
 module.exports=mincost;
